2fdde20511da54872affea3317baebaff35e09ab
Guillaume Bienkowski <guillaume.bienkowski@braincube.com>
2022-02-13 22:34:57 +0100

    TP data

---
+++ tp_securite/configuration.php
@@ -0,0 +1,120 @@
+<?php
+
+return [
+    'public_path' => APP_PUBLIC_PATH,
+    'public_dir' => APP_PUBLIC_DIR,
+    'overwrite_on_upload' => false,
+    'timezone' => 'Europe/Paris',
+    'download_inline' => ['pdf'],
+
+    'frontend_config' => [
+        'app_name' => 'Master4 Industries Dépôt de fichiers',
+        'app_version' => APP_VERSION,
+        'language' => 'french',
+        'upload_max_size' => 100 * 1024 * 1024, // 100MB
+        'upload_chunk_size' => 1 * 1024 * 1024, // 1MB
+        'upload_simultaneous' => 3,
+        'default_archive_name' => 'archive.zip',
+        'editable' => ['.txt'],
+        'date_format' => 'YY/MM/DD hh:mm:ss', // see: https://momentjs.com/docs/#/displaying/format/
+        'guest_redirection' => '', // useful for external auth adapters
+    ],
+
+    'services' => [
+        'Filegator\Services\Logger\LoggerInterface' => [
+            'handler' => '\Filegator\Services\Logger\Adapters\MonoLogger',
+            'config' => [
+                'monolog_handlers' => [
+                    function () {
+                        return new \Monolog\Handler\StreamHandler(
+                            '/private/logs/app.log',
+                            \Monolog\Logger::DEBUG
+                        );
+                    },
+                ],
+            ],
+        ],
+        'Filegator\Services\Session\SessionStorageInterface' => [
+            'handler' => '\Filegator\Services\Session\Adapters\SessionStorage',
+            'config' => [
+                'handler' => function () {
+                    $save_path = null; // use default system path
+                    //$save_path = __DIR__.'/private/sessions';
+                    $handler = new \Symfony\Component\HttpFoundation\Session\Storage\Handler\NativeFileSessionHandler($save_path);
+
+                    return new \Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage([], $handler);
+                },
+            ],
+        ],
+        'Filegator\Services\Cors\Cors' => [
+            'handler' => '\Filegator\Services\Cors\Cors',
+            'config' => [
+                'enabled' => APP_ENV == 'production' ? false : true,
+            ],
+        ],
+        'Filegator\Services\Tmpfs\TmpfsInterface' => [
+            'handler' => '\Filegator\Services\Tmpfs\Adapters\Tmpfs',
+            'config' => [
+                'path' => __DIR__.'/private/tmp/',
+                'gc_probability_perc' => 10,
+                'gc_older_than' => 60 * 60 * 24 * 2, // 2 days
+            ],
+        ],
+        'Filegator\Services\Security\Security' => [
+            'handler' => '\Filegator\Services\Security\Security',
+            'config' => [
+                'csrf_protection' => true,
+                'ip_allowlist' => [],
+                'ip_denylist' => [],
+            ],
+        ],
+        'Filegator\Services\View\ViewInterface' => [
+            'handler' => '\Filegator\Services\View\Adapters\Vuejs',
+            'config' => [
+                'add_to_head' => '',
+                'add_to_body' => '',
+            ],
+        ],
+        'Filegator\Services\Storage\Filesystem' => [
+            'handler' => '\Filegator\Services\Storage\Filesystem',
+            'config' => [
+                'separator' => '/',
+                'config' => [],
+                'adapter' => function () {
+                    $localStorage = new \League\Flysystem\Adapter\Local('/data');
+                    // composer require league/flysystem-sftp:^1.0 -W
+                    $backupStorage = new \League\Flysystem\Sftp\SftpAdapter([
+                        'host' => 'backup',
+                        'port' => 22,
+                        'username' => 'backup',
+                        'password' => 'B4ckUpP45s',
+                        'timeout' => 10,
+                    ]);
+                    // composer require league/flysystem-replicate-adapter
+                    return new League\Flysystem\Replicate\ReplicateAdapter($localStorage, $backupStorage);
+                },
+            ],
+        ],
+        'Filegator\Services\Archiver\ArchiverInterface' => [
+            'handler' => '\Filegator\Services\Archiver\Adapters\ZipArchiver',
+            'config' => [],
+        ],
+        'Filegator\Services\Auth\AuthInterface' => [
+            'handler' => '\Filegator\Services\Auth\Adapters\Database',
+            'config' => [
+                'driver' => 'mysqli',
+                'host' => 'database',
+                'username' => 'root',
+                'password' => 'Av3rYg0oDp4sZo4D',
+                'database' => 'authentication_depot',
+            ],
+        ],
+        'Filegator\Services\Router\Router' => [
+            'handler' => '\Filegator\Services\Router\Router',
+            'config' => [
+                'query_param' => 'r',
+                'routes_file' => __DIR__.'/backend/Controllers/routes.php',
+            ],
+        ],
+    ],
+];
---
+++ tp_securite/db/init.sql
@@ -0,0 +1,24 @@
+--
+CREATE TABLE `users` (
+    `id` int(10) NOT NULL AUTO_INCREMENT,
+    `username` varchar(255) NOT NULL,
+    `name` varchar(255) NOT NULL,
+    `role` varchar(20) NOT NULL,
+    `permissions` varchar(200) NOT NULL,
+    `homedir` varchar(2000) NOT NULL,
+    `password` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `username` (`username`)
+) CHARSET=utf8 COLLATE=utf8_bin;
+
+INSERT INTO `users`
+(`username`, `name`, `role`, `permissions`, `homedir`, `password`)
+VALUES
+('guest', 'Guest', 'guest', '', '/', ''),
+-- admin:An0tHerPassW0rd // hash généré via https://phppasswordhash.com/
+('admin', 'Admin', 'admin', 'read|write|upload|download|batchdownload|zip', '/', '$2y$10$n3fHgzxlXG/qnULh8BjMfuN8ysnV8kB8cse7XuB3M/BKoic7H9zDq'),
+-- fournisseur:HarDt0guEss // hash généré via https://phppasswordhash.com/
+('fournisseur', 'Fournisseurs', 'user', 'read|write|upload', '/', '$2y$10$bdk328EKlSqWnfUYCQDCi.8R77R/1lkYllSXsZnennhVn5bjdfQUK'),
+-- userinterne:SomeThinGh4sToGive // hash généré via https://phppasswordhash.com/
+('fournisseur', 'Fournisseurs', 'user', 'read|write|upload', '/', '$2y$10$GY2aC2EIaBvm6h48NfylW.dbA74TaMFykfvL4JHxBgDUq9rdwifpy')
+;
