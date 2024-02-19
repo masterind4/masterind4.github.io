9d12ba7b4cf930a111b646967ed510783e8a842e
Guillaume Bienkowski <guillaume@kdp.cloud>
2024-02-19 14:51:51 +0100

    Fix fournisseur

--- td_securite/db/init.sql
+++ td_securite/db/init.sql
@@ -20,6 +20,7 @@ VALUES
 ('admin', 'Admin', 'admin', 'read|write|upload|download|batchdownload|zip', '/', '$2y$10$n3fHgzxlXG/qnULh8BjMfuN8ysnV8kB8cse7XuB3M/BKoic7H9zDq'),
 -- fournisseur:HarDt0guEss // hash généré via https://phppasswordhash.com/
 -- "Homedir = /" => les fournisseurs voient tous les dossiers
+('fournisseur', 'Fournisseur', 'user', 'read|write|upload|download', '/', '$2y$10$bdk328EKlSqWnfUYCQDCi.8R77R/1lkYllSXsZnennhVn5bjdfQUK'),
 -- userinterne:SomeThinGh4sToGive // hash généré via https://phppasswordhash.com/
 ('userinterne', 'User Interne', 'user', 'read|write|upload|download', '/', '$2y$10$GY2aC2EIaBvm6h48NfylW.dbA74TaMFykfvL4JHxBgDUq9rdwifpy')
 ;
